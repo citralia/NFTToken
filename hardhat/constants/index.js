81de1e8456fa0340d1c7adedd33b11718cdb4a62
citralia <citralia77@outlook.com>
2023-03-11 16:29:41 +0000

    First Commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xD96f92179ff368a57916a0CC5Abf51654c450c83";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
